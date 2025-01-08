d1cccbb7572515d7aa7704e29c59a155c8fb65fd
prabhat-cn <prabhat@capitalnumbers.com>
2025-01-08 20:45:11 +0530

    Metadata SEO added

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,14 @@
+import { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+    // title: 'Blog- Page'
+    title: {
+        absolute: "Blog-Page-after adding absolute in the root layout metadata"
+    }
+  }
+
+
 export default function Blog() {
 
     return <h1>My Blog!</h1>
