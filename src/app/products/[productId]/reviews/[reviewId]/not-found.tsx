139950d865546153b937746240620135ebdd51c4
prabhat-cn <prabhat@capitalnumbers.com>
2025-01-08 19:22:10 +0530

    custom 404 and route level not found done

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+
+    return (
+        <div className="flex items-center justify-center h-screen">
+        <div className="text-center">
+            <h2 className="text-2xl font-bold">Review not found!</h2>
+            <p className="text-lg">Could not find requested resource from review!</p>
+        </div>
+    </div>
+    
+    );
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,5 @@
+import {notFound} from 'next/navigation';
+
 
 type productType = {
     params: {productId: string, reviewId: string,}
@@ -7,7 +9,10 @@ type productType = {
 // http://localhost:3000/products/(productId)/reviews/(reviewId)
 
     // http://localhost:3000/products/2/reviews/1
-    export default function ReviewDetail({ params }: productType) {
+export default function ReviewDetail({ params }: productType) {
+    if (parseInt(params.reviewId) > 1000) {
+        notFound();
+        }
         return (
     
             <>
