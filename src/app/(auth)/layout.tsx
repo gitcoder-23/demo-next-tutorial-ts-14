c770494330332d1a83564dc43a3f82805d73151d
prabhat-cn <prabhat@capitalnumbers.com>
2025-01-08 21:29:54 +0530

    active route added done

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+
+import Link from "next/link";
+import { usePathname } from 'next/navigation'
+import   './style.css'
+
+  
+const navLinks = [
+  { name: 'Register', href: '/register' },
+  { name: 'Login', href: '/login' },
+  {name: 'Forgot Password', href: '/forgot-password'},
+];
+
+
+
+export default function AuthLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+  const pathName = usePathname();
+  console.log('pathName=> ', pathName);
+  
+    return (
+      <div>
+        {
+          navLinks.map((link) => {
+            const isActive = pathName.startsWith(link.href);
+
+
+            return (
+              <Link className={isActive ? 'font-bold mr-4' : 'text-blue-500 mr-4'} href={link.href} key={link.name}> 
+              {link.name}
+              </Link>
+
+            );
+          })
+        }
+          {children}
+         
+      </div>
+        
+        
+    
+    )
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <h1>Welcome Home!</h1>
       <Link href="/blog">Blog</Link>&nbsp;
+      <Link href="/register">Register</Link>&nbsp;
       <Link href="/products">Products</Link>
     </div>
   );
