47a0c795a6262d6f8e43a46e2e45f5b226612b21
prabhat-cn <prabhat@capitalnumbers.com>
2025-01-08 20:32:23 +0530

    dynamic metadata added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,17 @@
 
+export const metaData = {
+    title: "About Next Tutorial",
+
+};
+
 export default function About() {
 
     return (
         <h1>About Us!</h1>
     );
 }
+
+
 
 
 
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,30 @@
+import { Metadata } from 'next'
 
- type productType = {
-params: {productId: string}
 
+type productType = {
+    params: {productId: string}
+    
+    }
+
+
+// generate dynamic metadata
+export const generateMetadata = async({ params }: productType): Promise<Metadata> => {
+   
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`);
+       }, 100)
+    });
+    return {
+        // title: `Product ${params.productId}`,
+        title: `Product ${title}`,
+    }
 }
+
+
+
+
+
 // http://localhost:3000/products/productId(dynamic)
 // http://localhost:3000/products/50
 export default function ProductNewDetails({ params }: productType) {
