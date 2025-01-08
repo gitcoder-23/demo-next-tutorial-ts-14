ff6743cb2d5d2398aa589af5a156cac2e3d486b7
prabhat-cn <prabhat@capitalnumbers.com>
2025-01-08 19:05:48 +0530

    Catch all full segments using [[...slug]] && Catch segments using [...slug]

---
+++ src/app/doc/[[...slug]]/page.tsx
@@ -0,0 +1,31 @@
+
+
+// Catch all full segments using [...slug]
+// http://localhost:3000/docs/{slug1}/{slug2}/{slug3}/
+// http://localhost:3000/docs/feature1/concept1/example1
+
+// http://localhost:3000/docs/routing/catch-all-segments
+
+
+// http://localhost:3000/docs/   ==== operable 
+type paramsSlugType = {
+    params: {slug: string[]}
+    
+    }
+
+ export default function Docs({params}: paramsSlugType) {
+        
+     if (params.slug?.length === 2) {
+         
+         return <h1>Viewing docs feature value {params.slug[0]} and concept {params.slug[1]}</h1>
+     } else if(params.slug?.length === 1) {
+        return <h1>Viewing docs feature New {params.slug[0]} </h1>
+         
+         
+     }
+     
+     return (
+            <h2>Docs Home Page</h2>     
+      
+        );
+    };
